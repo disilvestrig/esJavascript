dd1ef79b431b133858deaed5d4ae0f88338baa06
Giulio Di Silvestri <42960623+disilvestrig@users.noreply.github.com>
2020-11-29 17:04:00 +0100

    Update script6.js

--- script6.js
+++ script6.js
@@ -55,3 +55,95 @@ switch(+prompt("a?","")){
         alert('2,3')
         break
 }
+/*The following function returns true if the parameter age is greater than 18.
+
+Otherwise it asks for a confirmation and returns its result:
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    // ...
+    return confirm('Did parents allow you?');
+  }
+}
+Will the function work differently if else is removed?
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  }
+  // ...
+  return confirm('Did parents allow you?');
+}
+Is there any difference in the behavior of these two variants?
+*/ 
+// No fanno la stessa cosa
+
+
+
+
+/*The following function returns true if the parameter age is greater than 18.
+
+Otherwise it asks for a confirmation and returns its result.
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Did parents allow you?');
+  }
+}
+Rewrite it, to perform the same, but without if, in a single line.
+
+Make two variants of checkAge:
+
+Using a question mark operator ?
+Using OR ||*/
+
+
+function checkAge(age){
+  return (age > 18) ? true : confirm('Did parents allow you?')
+}
+
+function CheckAge(age){
+  return age > 18 || confirm("Did parents allow you?")
+}
+
+/*Write a function min(a,b) which returns the least of two numbers a and b.
+
+For instance:
+
+min(2, 5) == 2
+min(3, -1) == -1
+min(1, 1) == 1*/
+
+function min(a,b){
+  return (a > b) ? a : 
+         (a === b) ? "They are equal" : b
+}
+
+
+/*Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+
+pow(3, 2) = 3 * 3 = 9
+pow(3, 3) = 3 * 3 * 3 = 27
+pow(1, 100) = 1 * 1 * ...* 1 = 1
+Create a web-page that prompts for x and n, and then shows the result of pow(x,n).
+
+Run the demo
+
+P.S. In this task the function should support only natural values of n: integers up from 1.*/
+
+function getBase(){
+  return +prompt("Inserisci base")
+}
+function getEsponente(){
+  return +prompt("Inserisci esponente")
+}
+function power(a,b){
+  alert(a**b)
+}
+base = getBase()
+esponente = getEsponente()
+power(base,esponente)
